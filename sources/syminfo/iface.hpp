9a22aecc717f0e4cec670b0b282873761bd4563d
robkh <harya_krishnii@frtk.ru>
2013-03-09 20:55:00 +0400

    Added ELF and PE specification.

--- sources/syminfo/iface.hpp
+++ sources/syminfo/iface.hpp
@@ -15,15 +15,49 @@
 typedef QString symbolType;
 typedef void* addressType;
 
+/**
+ * Struct of symbol
+ */
+struct symbolType
+{
+    /**
+     * Name of symbol
+     */
+    QString name;
+
+    /**
+     * path to file
+     */
+    QString file;
+
+    /**
+     * line
+     */
+    int line;
+};
+
 class SymbolTableInterface 
 {
 public:
+    /**
+     * Initialize symbol table 
+     */
     virtual bool initialize() = 0;
+
     virtual bool finalize() = 0;
+
+    /** 
+     * Returns number of symbols
+     */
     virtual int getNumberOfSymbols() = 0;
+
     virtual void getListOfSymbols(symbolType* symbols) = 0;
+
     virtual int getNumberOfAddresses() = 0;
+
     virtual void getListOfAddresses(addressType* address) = 0;
+
     virtual symbolType getSymbol(addressType address) = 0;
+
     virtual addressType getAddress(symbolType symbol) = 0;
 }
